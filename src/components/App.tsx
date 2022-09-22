43ea9b6cc0868fd2393df588f5c1cbe2160d9a2d
imykhailychenko <igor.c.m@ukr.net>
2022-09-22 13:33:59 +0200

    fix ts error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
-import {Statistics} from "./Statistics/Statistics"
-import { FeedbackOptions } from "./Feedback/Feedback";
-import { Section } from "./Section/Section";
-import { Notification } from "./Notification/Notification";
-import React from "react"
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './Feedback/Feedback';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import React from 'react';
 
 interface IState {
   good: number,
@@ -10,39 +10,46 @@ interface IState {
   bad: number
 }
 
-export class App extends React.Component<unknown,IState> {
+export class App extends React.Component<unknown, IState> {
   state = {
-     good: 0,
-  neutral: 0,
-  bad: 0
-  }
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-  countTotalFeedback = ():number =>{
-    const result = Object.values(this.state)
-    return result.reduce((prev, el)  => {return prev + el},0)
-  } ;
-  
-  countPositiveFeedbackPercentage = ():number =>{
-    const result = this.state.good * 100 / this.countTotalFeedback()
-    return Math.round(result)
-  }
+  countTotalFeedback = (): number => {
+    const result = Object.values(this.state);
+    return result.reduce((prev, el) => {
+      return prev + el;
+    }, 0);
+  };
 
-  handleFeedback = (event: React.MouseEvent<HTMLButtonElement>) :void =>{
-    const { name } = event.target as EventTarget & { name: keyof IState }
+  countPositiveFeedbackPercentage = (): number => {
+    const result = this.state.good * 100 / this.countTotalFeedback();
+    return Math.round(result);
+  };
 
-    this.setState(ps => {return {[name]: ps[name] + 1} as Partial<IState>});
-  }
+  handleFeedback = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    const { name } = event.target as EventTarget & { name: keyof IState };
 
-  render ():JSX.Element {
-    const {good, neutral, bad} = this.state
+    this.setState(ps => {
+      return { [name]: ps[name] + 1 } as Readonly<IState>;
+    });
+  };
+
+  render(): JSX.Element {
+    const { good, neutral, bad } = this.state;
     return (<>
-      <Section title="Please leave feedback">
-      <FeedbackOptions onLeaveFeedback={this.handleFeedback} options={Object.keys(this.state)}/>
-      </Section>
-      <Section title="Statistics">
-      {this.countTotalFeedback() !== 0?<Statistics good={good} neutral={neutral} bad={bad} total ={this.countTotalFeedback()} percentage={this.countPositiveFeedbackPercentage()} />:<Notification message="There is no feedback"></Notification>}
-      </Section>
+        <Section title='Please leave feedback'>
+          <FeedbackOptions onLeaveFeedback={this.handleFeedback} options={Object.keys(this.state)} />
+        </Section>
+        <Section title='Statistics'>
+          {this.countTotalFeedback() !== 0 ?
+            <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()}
+                        percentage={this.countPositiveFeedbackPercentage()} /> :
+            <Notification message='There is no feedback'></Notification>}
+        </Section>
       </>
-    )
+    );
   }
 }
